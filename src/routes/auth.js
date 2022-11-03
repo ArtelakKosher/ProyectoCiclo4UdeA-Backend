a930aeb3f83dffeda3ad8f3bf099eaf264091073
MauricioV <mvargas.rodriguez@gmail.com>
2022-11-03 08:17:33 -0500

    Ajustes en rutas

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -15,14 +15,15 @@ const {
 const { isUserAuthenticated, authorizedRoles } = require("../middleware/auth");
 const router = express.Router();
 
+// Rutas gestionadas por user
 router.route("/user/register").post(userRegistration);
-router.route("/login").get(userLogin);
-router.route("/logout").get(isUserAuthenticated, logout);
-router.route("/passwordRecovery").post(passwordRecovery);
-router.route("/resetPassword/:token").post(resetPassword);
-router.route("/profile").get(isUserAuthenticated, getUserProfile);
-router.route("/profile/updatePassword").put(isUserAuthenticated, updatePassword);
-router.route("/profile/updateProfile").put(isUserAuthenticated, updateProfile);
+router.route("/user/login").get(userLogin);
+router.route("/user/logout").get(isUserAuthenticated, logout);
+router.route("/user/passwordRecovery").post(passwordRecovery);
+router.route("/user/resetPassword/:token").post(resetPassword);
+router.route("/user").get(isUserAuthenticated, getUserProfile);
+router.route("/user/updatePassword").put(isUserAuthenticated, updatePassword);
+router.route("/user/updateProfile").put(isUserAuthenticated, updateProfile);
 
 // Rutas gestionadas por admin
 router.route("/admin/users").get(isUserAuthenticated, authorizedRoles("admin"), getUsers);
